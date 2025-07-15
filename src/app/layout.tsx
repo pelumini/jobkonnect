9249a211da3a6b478459b0753d7f3b19f516f612
pelumini <pelumini@gmail.com>
2025-07-15 08:24:56 +0100

    sidebar and clerk auth added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import { ClerkProvider } from "@clerk/nextjs";
+import { ClerkProvider } from "@/services/clerk/components/ClerkProvider";
 import "./globals.css";
 import { Toaster } from "@/components/ui/sonner";
 
